2ca653f9e294f3edfae7fc25e53ca0e78ea07013
oscar morales <131509024+0skarmp@users.noreply.github.com>
2026-04-08 12:20:40 -0500

    redploy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full py-4 bg-gray-900 text-white text-center">
+        <footer className="w-full py-4 bg-gray-900 text-red-900 text-center">
         © 2026 — Made by Oskardev
         </footer>
     )
